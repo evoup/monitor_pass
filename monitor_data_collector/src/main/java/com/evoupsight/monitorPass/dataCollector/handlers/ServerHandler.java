ce7af277cca51ae6d6e50c1ae13573c4b46bf192
evoup.dell-notebook <evoex123@gmail.com>
2017-11-26 21:21:32 +0800

    fix log4j config problem

--- monitor_data_collector/src/main/java/com/evoupsight/monitorPass/dataCollector/handlers/ServerHandler.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorPass/dataCollector/handlers/ServerHandler.java
@@ -1,8 +1,11 @@
 package com.evoupsight.monitorPass.dataCollector.handlers;
 
+import com.evoupsight.monitorPass.dataCollector.server.Main;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -10,23 +13,24 @@ import org.springframework.stereotype.Component;
 @Qualifier("serverHandler")
 @Sharable
 public class ServerHandler extends SimpleChannelInboundHandler<String> {
-
+	private static final Logger LOG = LoggerFactory.getLogger(ServerHandler.class);
 	@Override
 	public void channelRead0(ChannelHandlerContext ctx, String msg)
 			throws Exception {
 		System.out.print(msg);
+		LOG.debug("got a message here");
 		ctx.channel().writeAndFlush(msg);
 	}
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		System.out.println("Channel is active\n");
+		LOG.debug("Channel is active");
 		super.channelActive(ctx);
 	}
 
 	@Override
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		System.out.println("\nChannel is disconnected");
+		LOG.debug("Channel is disconnected");
 		super.channelInactive(ctx);
 	}
 
