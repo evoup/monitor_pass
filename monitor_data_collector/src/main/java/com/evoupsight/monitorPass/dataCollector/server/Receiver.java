6c8af3a5d795ae28aaa681b7a826cfee27ff92e9
evoup.dell-notebook <evoex123@gmail.com>
2017-11-30 01:23:01 +0800

    add kafka for data aggreation

---
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorPass/dataCollector/server/Receiver.java
@@ -0,0 +1,27 @@
+package com.evoupsight.monitorPass.dataCollector.server;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Receiver {
+    public void recv() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        String itemKey = "madmonitor";
+        String itemVal = "load.avg.1min:0";
+        producer.send(new ProducerRecord<>("my-topic", itemKey, itemVal));
+        producer.close();
+    }
+}
