e3a32c827b476cfdaaf03819d7ff8d863b1dbd16
evoup.dell-notebook <evoex123@gmail.com>
2018-01-06 22:35:24 +0800

    add lost commit files

---
+++ madmonitor2/go/src/madmonitor2/utils/fs.go
@@ -0,0 +1,57 @@
+/*
+ * This file is part of madmonitor2.
+ * Copyright (c) 2017. Author: yinjia evoex123@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.  You should have received a copy
+ * of the GNU Lesser General Public License along with this program.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package utils
+
+import (
+	"os"
+)
+//Get file content
+func FileGetContent(filePath string) string {
+	logger := GetLogger()
+	bool_existed := FileExists(filePath)
+	if !bool_existed {
+		return ""
+	}
+	fin, err := os.Open(filePath)
+	defer fin.Close()
+	if err != nil {
+		Log(logger, "common.fs.FilePutContent][file path:"+filePath+"][open err",1, Debug_level)
+		os.Exit(0)
+	}
+	buf := make([]byte, 1024)
+	retStr := ""
+	for {
+		n, _ := fin.Read(buf)
+		if 0 == n {
+			break
+		}
+		retStr += string(buf[:n])
+	}
+	return retStr
+}
+
+func FilePutContent(fileName string, data string) bool {
+	logger := GetLogger()
+	Log(logger, "common.fs.FilePutContent][fileName:"+fileName,1, Debug_level)
+	fout, err := os.Create(fileName)
+	defer fout.Close()
+	if err != nil {
+		Log(logger, "common.fs.FilePutContent][err:"+err.Error(),1, Debug_level)
+		return false
+	}
+	fout.WriteString(data)
+	return true
+}
---
+++ madmonitor2/go/src/madmonitor2/utils/log.go
@@ -0,0 +1,39 @@
+/*
+ * This file is part of madmonitor2.
+ * Copyright (c) 2017. Author: yinjia evoex123@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.  You should have received a copy
+ * of the GNU Lesser General Public License along with this program.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package utils
+
+import (
+	"log"
+	"log/syslog"
+	"madmonitor2/inc"
+)
+
+
+// syslog init
+func GetLogger() *log.Logger {
+	Log, err := syslog.NewLogger(syslog.LOG_DEBUG, log.Lmicroseconds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return Log
+}
+
+// use syslog log item
+func Log(l *log.Logger, s string, debug_level_orig int, debug_level_input int) {
+	if (debug_level_orig<=debug_level_input) {
+		l.Print("[" + s + "][" + inc.LOG_SUFFIX + "." + inc.CLIENT_VERSION + "]")
+	}
+}
