edeb98281d633cfe9411a970a84c1acdbefb5531
evoup <evoex123@gmail.com>
2018-04-22 00:26:03 +0800

    add build script

---
+++ madmonitor2/go/src/madmonitor2/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+go build madmonitor2.go
+go build --buildmode=plugin -o /usr/local/lib/madmonitor2/sysload.so collectors/sysload.go                                    
+go build --buildmode=plugin -o /usr/local/lib/madmonitor2/ifstat.so collectors/ifstat.go                                      
+go build --buildmode=plugin -o /usr/local/lib/madmonitor2/dfstat.so collectors/dfstat.go                                      
+go build --buildmode=plugin -o /usr/local/lib/madmonitor2/procstats.so collectors/procstats.go
