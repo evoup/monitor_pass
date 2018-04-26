79e5690bad66f76184143e563b9ec454c86a1f97
evoup <evoex123@gmail.com>
2018-04-26 17:42:27 +0800

    add opentsdb-client support

--- monitor_data_collector/src/main/java/com/evoupsight/monitorPass/dataCollector/services/ConsumerService.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorPass/dataCollector/services/ConsumerService.java
@@ -2,12 +2,18 @@ package com.evoupsight.monitorPass.dataCollector.services;
 
 import com.evoupsight.kafkaclient.consumer.KafkaConsumer;
 import com.evoupsight.kafkaclient.util.KafkaCallback;
+import org.opentsdb.client.ExpectResponse;
+import org.opentsdb.client.HttpClient;
+import org.opentsdb.client.HttpClientImpl;
+import org.opentsdb.client.builder.MetricBuilder;
+import org.opentsdb.client.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +37,29 @@ public class ConsumerService {
                 String msg = new String(message);
                 System.out.println(String.format("%s-%d-%d-%s", topic, partition, offset, msg));
                 messageBag.add(msg);
-                if (messageBag.size() > 10) {
+                // TODO 这里改成如果时间超过指定时间的话也要发送掉
+                if (messageBag != null && messageBag.size() > 10) {
                     System.out.println("send all messages");
+                    ////////
+                    HttpClient client = new HttpClientImpl("http://localhost:8242");
+
+
+                    MetricBuilder builder = MetricBuilder.getInstance();
+
+                    builder.addMetric("metric1").setDataPoint(2, 30L)
+                            .addTag("tag1", "tab1value").addTag("tag2", "tab2value");
+
+                    builder.addMetric("metric2").setDataPoint(2, 232.34)
+                            .addTag("tag3", "tab3value");
+
+                    try {
+                        Response response = client.pushMetrics(builder,
+                                ExpectResponse.SUMMARY);
+                        System.out.println(response);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    ////////
                     messageBag = new ArrayList<>();
                 }
                 return true;
