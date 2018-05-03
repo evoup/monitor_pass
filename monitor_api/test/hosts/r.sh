ac086f0f973cd119469ccaf841eb0f53b81c14b4
evoup.cop-macosx <evoex123@gmail.com>
2018-05-03 14:06:47 +0800

    c

---
+++ monitor_api/test/hosts/r.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+. ../include/cookie.sh
+curl -v 'http://localhost:8004/mmsapi1.0/get/host/@self/monitorserver' -H "Cookie: __CO_MMSUNAME=${__CO_MMSUNAME}; __CO_MMSUID=${__CO_MMSUID}" -H 'Origin: http://localhost:8004' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: zh-CN,zh;q=0.9' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Referer: http://localhost:8004/monitor_ui/client/configure/add_edit_usergroup.html?version=1.0' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed
