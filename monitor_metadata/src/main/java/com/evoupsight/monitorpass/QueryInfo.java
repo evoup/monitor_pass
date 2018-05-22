e1a65d493d5ed5ac6bc16efd0ddc3e176ffabf75
yinjia <yinjia@madhouse-inc.com>
2018-05-22 10:51:19 +0800

    remove useless code

--- monitor_metadata/src/main/java/com/evoupsight/monitorpass/QueryInfo.java
+++ monitor_metadata/src/main/java/com/evoupsight/monitorpass/QueryInfo.java
@@ -2,20 +2,15 @@ package com.evoupsight.monitorpass;
 
 import com.evoupsight.monitorpass.domain.Item;
 import com.google.gson.Gson;
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.PageFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author evoup
@@ -46,33 +41,26 @@ public class QueryInfo {
 
     public void getTables() {
         Configuration config = getHbaseConf();
-//            byte[] row1 = Bytes.toBytes("23219");
-//            TableName table1 = TableName.valueOf("monitor_items");
-//            String family1 = "info";
-//            Get g = new Get(row1);
-//            System.out.println(table1.getName());
+        // Instantiate HBaseAdmin class
+        HBaseAdmin ad = null;
+        try {
+            ad = new HBaseAdmin(config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            // Instantiate HBaseAdmin class
-            HBaseAdmin ad = null;
-            try {
-                ad = new HBaseAdmin(config);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        //  Give list of tables HBaseAdmin Object
+        HTableDescriptor[] tDescriptor = new HTableDescriptor[0];
+        try {
+            tDescriptor = ad.listTables();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            //  Give list of tables HBaseAdmin Object
-            HTableDescriptor[] tDescriptor = new HTableDescriptor[0];
-            try {
-                tDescriptor = ad.listTables();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            for (int k=0; k<tDescriptor.length;k++ ) {
-
-                // Print all the table names.
-                System.out.println(tDescriptor[k].getNameAsString());
-            }
+        for (int k = 0; k < tDescriptor.length; k++) {
+            // Print all the table names.
+            System.out.println(tDescriptor[k].getNameAsString());
+        }
 
     }
 
@@ -93,115 +81,77 @@ public class QueryInfo {
      */
     public String scanData(HBaseAdmin ad) throws IOException {
         Connection connection = ConnectionFactory.createConnection(ad.getConfiguration());
-        //List<RowValue> result = new ArrayList<>();
-        //try (Connection connection = hBaseConnectionFactory.connect()) {
         Table table = connection.getTable(TableName.valueOf("monitor_items"));
         Scan scan = new Scan();
         //scan.setFilter(new PageFilter(10));
         HashMap<String, Item> confMap = new HashMap();
         try (ResultScanner rs = table.getScanner(scan)) {
             for (Result r = rs.next(); r != null; r = rs.next()) {
-                //conversionsService.constructRowValue is a helper method (defined in the app)
-                //result.add(conversionsService.constructRowValue(r));
-                //family
-                //qualifier
-                //System.out.println(r.getColumnCells("info".getBytes(), "desc".getBytes()));
-                //System.out.println(r.getColumnCells("info".getBytes(), "description".getBytes()));
-                //System.out.println(r.getColumnCells("info".getBytes(), "data_type".getBytes()));
-                //System.out.println(r.getColumnCells("info".getBytes(), "delay".getBytes()));
-                //List<Cell> columnCells = r.getColumnCells("info".getBytes(), "desc".getBytes());
                 byte[] row = r.getRow();
-                if (row != null) {
-                    //System.out.println("row:" + new String(row));
-                }
                 Item item = new Item();
                 byte[] value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("type"));
                 if (value != null) {
-                    //System.out.println("[info:type]:" + new String(value));
                     item.setType(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("data_type"));
                 if (value != null) {
-                    //System.out.println("[info:data_type]:" + new String(value));
                     item.setDataType(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("snmp_community"));
                 if (value != null) {
-                    //System.out.println("[info:snmp_community]:" + new String(value));
                     item.setSnmpCommunity(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("snmp_oid"));
                 if (value != null) {
-                    //System.out.println("[info:snmp_oid]:" + new String(value));
                     item.setSnmpOid(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("host_id"));
                 if (value != null) {
-                    //System.out.println("[info:host_id]:" + new String(value));
                     item.setHostId(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("name"));
                 if (value != null) {
-                    //System.out.println("[info:name]:" + new String(value));
                     item.setName(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("key_"));
                 if (value != null) {
-                    //System.out.println("[info:key_]:" + new String(value));
                     item.setKey(new String(value));
-                }
-                value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("desc"));
-                if (value != null) {
-                    //System.out.println("[info:desc]:" + new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("delay"));
                 if (value != null) {
-                    //System.out.println("[info:delay]:" + new String(value));
                     item.setDelay(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("history"));
                 if (value != null) {
-                    //System.out.println("[info:history]:" + new String(value));
                     item.setHistory(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("status"));
                 if (value != null) {
-                    //System.out.println("[info:status]:" + new String(value));
                     item.setStatus(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("value_type"));
                 if (value != null) {
-                    //System.out.println("[info:value_type]:" + new String(value));
                     item.setValueType(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("units"));
                 if (value != null) {
-                    //System.out.println("[info:units]:" + new String(value));
                     item.setUnits(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("multiplier"));
                 if (value != null) {
-                    //System.out.println("[info:multiplier]:" + new String(value));
                     item.setMultiplier(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("delta"));
                 if (value != null) {
-                    //System.out.println("[info:delta]:" + new String(value));
                     item.setDelta(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("lastlogsize"));
                 if (value != null) {
-                    //System.out.println("[info:lastlogsize]:" + new String(value));
                     item.setLastlogsize(new String(value));
                 }
                 value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("data_type"));
                 if (value != null) {
-                    //System.out.println("[info:data_type]:" + new String(value));
                     item.setDataType(new String(value));
-                }
-                value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("description"));
-                if (value != null) {
-                    //System.out.println("[info:description]:" + new String(value));
                 }
                 if (row != null) {
                     confMap.put(new String(row), item);
@@ -217,9 +167,9 @@ public class QueryInfo {
      * 根据RowKey获取数据
      *
      * @param tableName 表名称
-     * @param rowKey RowKey名称
+     * @param rowKey    RowKey名称
      * @param colFamily 列族名称
-     * @param col 列名称
+     * @param col       列名称
      * @throws IOException
      */
     public void getData(String tableName, String rowKey, String colFamily, String col, HBaseAdmin ad) throws IOException {
