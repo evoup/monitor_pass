f3152188a29ba4f01bd884accb37eb330b49afc3
evoup <evoex123@gmail.com>
2018-06-10 14:56:10 +0800

    change opentsdb key to dot notation style

--- monitor_server/DataBase/hbase_table.php
+++ monitor_server/DataBase/hbase_table.php
@@ -14,8 +14,8 @@
   +----------------------------------------------------------------------+
  */
 define(__THRIFT_ROOT,'../GPL/thrift');
-define(__MDB_HOST,'10.1.1.118');
-define(__MDB_PORT,'32811');
+define(__MDB_HOST,'192.168.2.197');
+define(__MDB_PORT,'32777');
 define(__MDB_SENDTIMEOUT, '20000');  //10 seconds
 define(__MDB_RECVTIMEOUT, '20000');  //10 seconds
 define(__TABLE1_NAME,     'monitor_servername'); //被监控服务器名表
