ec2b75d06e67e60038028e74e1098bb12e2a7585
evoup <evoex123@gmail.com>
2018-06-17 22:26:56 +0800

    c

--- server2/src/main/java/com/evoupsight/monitorpass/server/services/ScanEventTask.java
+++ server2/src/main/java/com/evoupsight/monitorpass/server/services/ScanEventTask.java
@@ -1,13 +1,15 @@
 package com.evoupsight.monitorpass.server.services;
 
 
+import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessLock;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,37 +26,34 @@ import static com.evoupsight.monitorpass.constants.Constants.*;
 public class ScanEventTask {
     private static final Logger LOG = LoggerFactory.getLogger(ScanEventTask.class);
 
-
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
 
-
-
-    private final
-    CuratorFramework cfClient;
-
-    @Autowired
-    public ScanEventTask(@Qualifier("cfClient") CuratorFramework cfClient) {
-        this.cfClient = cfClient;
-        this.cfClient.start();
-    }
+    @Value("${zk.servers}")
+    String zkServers;
 
     @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() throws Exception {
-        InterProcessLock lock = new InterProcessMutex(cfClient, LOCK_PATH);
-        if (lock.acquire(DEFAULT_WAIT_TIME_SECONDS, TimeUnit.SECONDS)) {
-            try {
-                //doSomeWork(myName);
-                LOG.info("doSomeWork");
-                Thread.sleep(15000);
-            } finally {
-                lock.release();
+    public void reportCurrentTime() {
+        int baseSleepTimeMills = 1000;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMills, maxRetries);
+        try (CuratorFramework cfClient = CuratorFrameworkFactory.newClient(zkServers, retryPolicy)) {
+            cfClient.start();
+            InterProcessLock lock = new InterProcessMutex(cfClient, LOCK_PATH);
+            if (lock.acquire(DEFAULT_WAIT_TIME_SECONDS, TimeUnit.SECONDS)) {
+                try {
+                    //doSomeWork(myName);
+                    LOG.info("doSomeWork");
+                    Thread.sleep(15000);
+                } finally {
+                    lock.release();
+                }
+            } else {
+                LOG.error("{} timed out after {} seconds waiting to acquire lock on {}",
+                        MY_NAME, DEFAULT_WAIT_TIME_SECONDS, LOCK_PATH);
             }
-        } else {
-            LOG.error("{} timed out after {} seconds waiting to acquire lock on {}",
-                    MY_NAME, DEFAULT_WAIT_TIME_SECONDS, LOCK_PATH);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
         }
-
-//        cfClient.close();
 
         LOG.info("The time is now {}", DATE_FORMAT.format(new Date()));
     }
