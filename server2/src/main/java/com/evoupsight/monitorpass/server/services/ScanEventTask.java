06184f1cc16feb7871379e311ba439acb5efb5a4
evoup <evoex123@gmail.com>
2018-06-17 20:16:59 +0800

    add curator

--- server2/src/main/java/com/evoupsight/monitorpass/server/services/ScanEventTask.java
+++ server2/src/main/java/com/evoupsight/monitorpass/server/services/ScanEventTask.java
@@ -4,7 +4,14 @@ package com.evoupsight.monitorpass.server.services;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,12 +19,38 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScanEventTask {
-    private static final Logger log = LoggerFactory.getLogger(ScanEventTask.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ScanEventTask.class);
+    private static final long DEFAULT_WAIT_TIME_SECONDS = Long.MAX_VALUE;
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    public void reportCurrentTime() throws Exception {
+        String hosts = "zk1:2181,zk2:2181,zk3:2181";
+        String lockPath = "/hehe";
+        String myName = "test";
+        long waitTimeSeconds = DEFAULT_WAIT_TIME_SECONDS;
+        int baseSleepTimeMills = 1000;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(baseSleepTimeMills, maxRetries);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(hosts, retryPolicy);
+        client.start();
+        InterProcessLock lock = new InterProcessMutex(client, lockPath);
+        if (lock.acquire(waitTimeSeconds, TimeUnit.SECONDS)) {
+            try {
+                //doSomeWork(myName);
+                LOG.info("doSomeWork");
+                Thread.sleep(15000);
+            } finally {
+                lock.release();
+            }
+        } else {
+            LOG.error("{} timed out after {} seconds waiting to acquire lock on {}",
+                    myName, waitTimeSeconds, lockPath);
+        }
+
+        client.close();
+
+        LOG.info("The time is now {}", dateFormat.format(new Date()));
     }
 }
