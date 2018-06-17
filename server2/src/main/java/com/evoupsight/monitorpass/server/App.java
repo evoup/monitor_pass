dddef9c72b71c82ae6e0d9cdc1c1893702e1fcc3
evoup <evoex123@gmail.com>
2018-06-17 12:36:14 +0800

    c

---
+++ server2/src/main/java/com/evoupsight/monitorpass/server/App.java
@@ -0,0 +1,14 @@
+package com.evoupsight.monitorpass.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(scanBasePackages="com.evoupsight.monitorpass.server")
+@EnableScheduling
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
