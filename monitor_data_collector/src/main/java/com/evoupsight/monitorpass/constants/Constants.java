5d9552a621b20a07c8b1a1307dcd6b8a4cbc9e2d
evoup <evoex123@gmail.com>
2018-06-18 22:50:22 +0800

    c

---
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/constants/Constants.java
@@ -0,0 +1,6 @@
+package com.evoupsight.monitorpass.constants;
+
+public class Constants {
+    public static final String MDB_TAB_HOSTS = "monitor_hosts";
+    public static final String MDB_COL_HOST_LAST_IN = "info:lastin";
+}
