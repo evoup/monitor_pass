3e8ed3a736e01b3e162840523add6154b7b923b0
evoup <evoex123@gmail.com>
2018-06-19 00:39:13 +0800

    c

--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/utils/HbaseUtils.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/utils/HbaseUtils.java
@@ -52,7 +52,7 @@ public class HbaseUtils {
             connection = ConnectionFactory.createConnection(ad.getConfiguration());
             table = connection.getTable(TableName.valueOf(MDB_TAB_HOSTS));
             Put p = new Put(Bytes.toBytes(host));
-            p.addColumn(Bytes.toBytes("info"), Bytes.toBytes("lastin"), Bytes.toBytes(System.currentTimeMillis()));
+            p.addColumn(Bytes.toBytes("info"), Bytes.toBytes("lastin"), Bytes.toBytes(String.valueOf(System.currentTimeMillis())));
             table.put(p);
             LOG.info("host:" + host + "added to hbase");
         } catch (IOException e) {
