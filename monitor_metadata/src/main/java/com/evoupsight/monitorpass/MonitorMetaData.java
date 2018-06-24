d90be9309ecadc267e0fb69641d8ec9023355944
evoup <evoex123@gmail.com>
2018-06-24 23:59:35 +0800

    删除enableAutoconfiguration,去掉assembly plugin,springboot-maven-plugin自己可以做成jar

--- monitor_metadata/src/main/java/com/evoupsight/monitorpass/MonitorMetaData.java
+++ monitor_metadata/src/main/java/com/evoupsight/monitorpass/MonitorMetaData.java
@@ -3,20 +3,16 @@ package com.evoupsight.monitorpass;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 
 
 /**
  * @author evoup
  */
 @SpringBootApplication(scanBasePackages="com.evoupsight.monitorpass")
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class, ErrorWebFluxAutoConfiguration.class,
-        EmbeddedWebServerFactoryCustomizerAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class, ErrorWebFluxAutoConfiguration.class,
+//        EmbeddedWebServerFactoryCustomizerAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class})
 public class MonitorMetaData implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MonitorMetaData.class);
