18eda193f774954e7542bc6acf60d3973208f23b
evoup <evoex123@gmail.com>
2018-06-25 22:17:40 +0800

    fix get metadata column bug

--- monitor_metadata/src/main/java/com/evoupsight/monitorpass/services/QueryInfoService.java
+++ monitor_metadata/src/main/java/com/evoupsight/monitorpass/services/QueryInfoService.java
@@ -100,9 +100,6 @@ public class QueryInfoService {
             for (Result r = rs.next(); r != null; r = rs.next()) {
                 byte[] row = r.getRow();
                 String hostName = new String(row);
-                LOG.info("host:" + hostName);
-                byte[] value = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("type"));
-                LOG.info("value:" + value);
                 // 数据收集器不为空，说明是实际的服务器
                 if (r.getValue(Bytes.toBytes("info"), Bytes.toBytes("data_collector")) != null) {
                     byte[] templateBytes = r.getValue(Bytes.toBytes("info"), Bytes.toBytes("template"));
