dbd36f9418377b0a2f16d97cec6f8da812ac6376
evoup <evoex123@gmail.com>
2018-08-12 16:47:17 +0800

    添加注释

--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/auth/exception/InvalidProtocolException.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/auth/exception/InvalidProtocolException.java
@@ -1,4 +1,27 @@
 package com.evoupsight.monitorpass.datacollector.auth.exception;
 
+/**
+ * @author evoup
+ */
 public class InvalidProtocolException extends Exception {
+    public InvalidProtocolException() {
+        super();
+    }
+
+    public InvalidProtocolException(String message) {
+        super(message);
+    }
+
+    public InvalidProtocolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidProtocolException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InvalidProtocolException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/server/ServerState.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/server/ServerState.java
@@ -1,8 +1,22 @@
 package com.evoupsight.monitorpass.datacollector.server;
 
+/**
+ * @author evoup
+ */
+
 public enum ServerState {
+    /**
+     * 初始化
+     */
     INITIAL,
+    /**
+     * 客户端首个消息已经处理
+     */
     FIRST_CLIENT_MESSAGE_HANDLED,
+    @Deprecated
     PREPARED_FIRST,
+    /**
+     * 认证结束
+     */
     ENDED
 }
