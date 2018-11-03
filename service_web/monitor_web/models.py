54a45938e8bd664f7cfb46f133d9665c2ffbf635
evoup <evoex123@gmail.com>
2018-11-03 22:20:31 +0800

    添加server模型

--- service_web/monitor_web/models.py
+++ service_web/monitor_web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ServerModels(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40, null=False)
+    ip = models.CharField(max_length=20, null=False)
