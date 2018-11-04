224457071a19c3d0a13e2e2a142e388cc082f0fc
evoup <evoex123@gmail.com>
2018-11-04 23:03:34 +0800

    提交映射文件

--- service_web/monitor_web/models.py
+++ service_web/monitor_web/models.py
@@ -3,7 +3,26 @@ from django.db import models
 # Create your models here.
 
 
-class ServerModels(models.Model):
+class UserProfile(models.Model):
+    """
+    用户信息
+    """
+    name = models.CharField(u'姓名', max_length=32)
+    email = models.EmailField(u'邮箱')
+    telephone = models.CharField(u'座机', max_length=32)
+    mobile = models.IntegerField(u'手机')
+
+    class Meta:
+        verbose_name_plural = '用户信息表'
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40, null=False)
     ip = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.name
