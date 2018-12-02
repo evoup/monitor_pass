1f1a8e9e52b1518db91e4b6a51a99f1fe91074e2
evoup <evoex123@gmail.com>
2018-12-02 22:35:00 +0800

    增加若干张表

--- monitor_api2/monitor_web/admin.py
+++ monitor_api2/monitor_web/admin.py
@@ -5,3 +5,12 @@ from monitor_web import models
 
 admin.site.register(models.UserProfile)
 admin.site.register(models.Server)
+admin.site.register(models.DataCollector)
+admin.site.register(models.Event)
+admin.site.register(models.UserGroup)
+admin.site.register(models.BusinessUnit)
+admin.site.register(models.IDC)
+admin.site.register(models.Tag)
+admin.site.register(models.Asset)
+
+
--- monitor_api2/monitor_web/models.py
+++ monitor_api2/monitor_web/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -21,8 +22,8 @@ class UserProfile(models.Model):
 
 class Server(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=40, null=False)
-    ip = models.CharField(max_length=20, null=False)
+    name = models.CharField(u'服务器主机名', max_length=40, null=False)
+    ip = models.CharField(u'IP地址', max_length=20, null=False)
 
     class Meta:
         # ordering = ('id',)
@@ -30,3 +31,125 @@ class Server(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class DataCollector(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(u'数据收集器名', max_length=40, null=False)
+    ip = models.CharField(u'IP地址', max_length=20, null=False)
+    port = models.IntegerField(u'端口号')
+    desc = models.CharField(u'描述', max_length=50, null=True)
+
+    class Meta:
+        # ordering = ('id',)
+        verbose_name_plural = '数据收集器表'
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    event = models.CharField(max_length=200, null=False)
+    date = models.DateTimeField()
+    host_id = models.ForeignKey('Server', on_delete=models.CASCADE)
+    class Meta:
+        # ordering = ('id',)
+        verbose_name_plural = '监控事件表'
+
+    def __str__(self):
+        return self.event
+
+
+class UserGroup(models.Model):
+    """
+    用户组
+    """
+    name = models.CharField(max_length=32, unique=True)
+    users = models.ManyToManyField('UserProfile')
+
+    class Meta:
+        verbose_name_plural = "用户组表"
+
+    def __str__(self):
+        return self.name
+
+
+class BusinessUnit(models.Model):
+    """
+    业务线
+    """
+    name = models.CharField('业务线', max_length=64, unique=True)
+    contact = models.ForeignKey('UserGroup', verbose_name='业务联系人', related_name='c', on_delete=models.CASCADE)  # 多个人
+    manager = models.ForeignKey('UserGroup', verbose_name='系统管理员', related_name='m', on_delete=models.CASCADE)  # 多个人
+
+    class Meta:
+        verbose_name_plural = "业务线表"
+
+    def __str__(self):
+        return self.name
+
+
+class IDC(models.Model):
+    """
+    机房信息
+    """
+    name = models.CharField('机房', max_length=32)
+    floor = models.IntegerField('楼层', default=1)
+
+    class Meta:
+        verbose_name_plural = "机房表"
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """
+    资产标签
+    """
+    name = models.CharField('标签', max_length=32, unique=True)
+
+    class Meta:
+        verbose_name_plural = "标签表"
+
+    def __str__(self):
+        return self.name
+
+
+class Asset(models.Model):
+    """
+    资产信息表，所有资产公共信息（交换机，服务器，防火墙等）
+    """
+    device_type_choices = (
+        (1, '服务器'),
+        (2, '交换机'),
+        (3, '防火墙'),
+    )
+    device_status_choices = (
+        (1, '上架'),
+        (2, '在线'),
+        (3, '离线'),
+        (4, '下架'),
+    )
+
+    device_type_id = models.IntegerField(choices=device_type_choices, default=1)
+    device_status_id = models.IntegerField(choices=device_status_choices, default=1)
+
+    cabinet_num = models.CharField('机柜号', max_length=30, null=True, blank=True)
+    cabinet_order = models.CharField('机柜中序号', max_length=30, null=True, blank=True)
+
+    idc = models.ForeignKey('IDC', verbose_name='IDC机房', null=True, blank=True, on_delete=models.CASCADE)
+    business_unit = models.ForeignKey('BusinessUnit', verbose_name='属于的业务线', null=True, blank=True,
+                                      on_delete=models.CASCADE)
+
+    tag = models.ManyToManyField('Tag')
+
+    latest_date = models.DateField(null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "资产表"
+
+    def __str__(self):
+        return "%s-%s-%s" % (self.idc.name, self.cabinet_num, self.cabinet_order)
