bf560ff9376cd46de406d1df2c19b73c5a105e00
evoup <evoex123@gmail.com>
2018-12-02 19:40:02 +0800

    api login post使用jwt的token /6 过期自动重定向到登录页

--- monitor_ui2/client/js/main.js
+++ monitor_ui2/client/js/main.js
@@ -1,80 +1,71 @@
-function get_status_logininfo_Callback(json, textStatus, jqXHR){
-  if(json==null){
-    console.log("json null");
-	}else{
-    //console.log(json);
-		var account,ver=url_params("version");
-	  if(json[1]==null){
-	    window.location.href="login.html?version="+ver;
-	  }else{
-	     if(json[0]==null){
-		     account=json[1];
-		    }else{
-		     account=json[0]+"："+json[1];
+function get_status_logininfo_Callback(json, textStatus, jqXHR) {
+    if (json == null) {
+        console.log("json null");
+    } else {
+        var account, ver = url_params("version");
+        if (json[1] == null) {
+            window.location.href = "login.html?version=" + ver;
+        } else {
+            if (json[0] == null) {
+                account = json[1];
+            } else {
+                account = json[0] + "：" + json[1];
+            }
+            $("#header .account cite").html(account);
         }
-		 $("#header .account cite").html(account);
     }
-	 //account=json[0]+"："+json[1];
-	 //console.log(account);
-	 //$(".header .account cite").html(account);
-	}
 }
 
 
-
-
-function exitLogin(_url){
- var vers=url_params("version");
- $.ajax({
-      type: "get",
-       url:  _url,
-     async:  true,
-  dataType: "json",
-   success: function(json, textStatus, jqXHR){
-            //alert(jqXHR.status+"s");
-	 },
-  error: function(jqXHR, textStatus, errorThrown){
-		 if(jqXHR.status==401){
-			 window.location.href="login.html?version="+vers;
-       }
-      }
-   });
+function exitLogin(_url) {
+    var vers = url_params("version");
+    $.ajax({
+        type: "post",
+        url: _url,
+        async: false,
+        data: {'token': window.localStorage['mms_token']},
+        success: function (json, textStatus, jqXHR) {
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status == 401 || jqXHR.status == 400) {
+                window.location.href = "login.html?version=" + vers;
+            }
+        }
+    });
 }
 
 
-function contentIframeHeight(){ 
-	//contentIframe Height
-  var notIframeHeight=$("#header").height()+$("#top_navigation").height()+$("#footer").height()+30,
-	    iframeHeight=$(window).height()-notIframeHeight;
-	 //console.log(iframeHeight);
-	 $("#contentIframe").height(iframeHeight);
+function contentIframeHeight() {
+
+    var notIframeHeight = $("#header").height() + $("#top_navigation").height() + $("#footer").height() + 30,
+        iframeHeight = $(window).height() - notIframeHeight;
+
+    $("#contentIframe").height(iframeHeight);
 }
 
-$(document).ready(function(){
-  var version=url_params("version");
-	var domain=domainURI(document.location.href);
-	//console.log(version);
-	bindClickNav();
+$(document).ready(function () {
+    var version = url_params("version");
+    var domain = domainURI(document.location.href);
 
-	var yearLastVersion=new Date().getFullYear(); //页脚版本结束年份
-	//console.log(yearLastVersion);
-	$("#footer cite").text(yearLastVersion);
-  
-	get_data_notLoad("http://"+domain+"/mmsapi"+version+"/get/status/@logininfo", get_status_logininfo_Callback);
-  $("#header .account a").click(function(){
-  	// exitLogin("http://"+domain+"/mmsapi"+version+"/delete/login/@self");
-	  window.localStorage['mms_token']=null;
-	  window.location.href="login.html?version="+version;
-  });
-	$("#contentIframe").load(function(){
-	   contentIframeHeight();	
-	}); 
-  //contentIframeHeight();
-  window.onresize=contentIframeHeight;
-  //console.log(document.documentElement.clientHeight+","+$("#top_navigation").height()+","+$("#header").height()+","+$("#footer").height());
-  exitLogin("http://"+domain+"/mmsapi"+version+"/get/login/@self");
+    bindClickNav();
 
-	window.setInterval(function(){
-		exitLogin("http://"+domain+"/mmsapi"+version+"/get/login/@self");
-	},300000);
+    var yearLastVersion = new Date().getFullYear(); //页脚版本结束年份
+
+    $("#footer cite").text(yearLastVersion);
+
+    get_data_notLoad("http://" + domain + "/mmsapi" + version + "/get/status/@logininfo", get_status_logininfo_Callback);
+    $("#header .account a").click(function () {
+        window.localStorage['mms_token'] = null;
+        window.location.href = "login.html?version=" + version;
+    });
+    $("#contentIframe").load(function () {
+        contentIframeHeight();
+    });
+    window.onresize = contentIframeHeight;
+
+    exitLogin("http://" + domain + "/mmsapi" + version + "/login/status/");
+
+    window.setInterval(function () {
+        exitLogin("http://" + domain + "/mmsapi" + version + "/login/status/");
+    }, 300000);
 });
