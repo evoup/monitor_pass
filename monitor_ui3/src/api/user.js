a142794fd6c1ab2c475d9906b7f0598c84f6fb63
evoup.dell-notebook <evoex123@gmail.com>
2019-04-11 00:43:30 +0800

    用户组表界面
    用户组界面

---
+++ monitor_ui3/src/api/user.js
@@ -0,0 +1,22 @@
+import request from '@/utils/request'
+
+export function add_usergroup(name, desc, priv, members) {
+  return request({
+    url: '/user_group/info',
+    method: 'post',
+    data: {
+      name,
+      desc,
+      priv,
+      members
+    }
+  })
+}
+
+export function user_group_list(param) {
+  return request({
+    url: 'user_group/list',
+    method: 'get',
+    param: param
+  })
+}
