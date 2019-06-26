0740d4b7876fa4718831a9999d6f6109e77c6b17
evoup.dell-notebook <evoex123@gmail.com>
2019-06-27 00:24:43 +0800

    删除服务器组完成

--- monitor_ui3/src/views/server/server_group_list.vue
+++ monitor_ui3/src/views/server/server_group_list.vue
@@ -51,7 +51,7 @@
       <el-table-column label="操作">
         <template slot-scope="prop">
           <el-button size="small" type="primary" @click="lookUser(prop.$index,prop.row.u_uuid)">查看</el-button>
-          <el-button size="small" type="danger" @click="deleteTemplate(prop.row.id, prop.$index)">删除</el-button>
+          <el-button size="small" type="danger" @click="deleteServerGroup(prop.row.id, prop.$index)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
