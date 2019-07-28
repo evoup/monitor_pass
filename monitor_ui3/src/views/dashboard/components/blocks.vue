e42f9395573339d96a833581e78f0bff3b5bb431
evoup.dell-notebook <evoex123@gmail.com>
2019-07-28 18:15:38 +0800

    dragable初步实现

--- monitor_ui3/src/views/dashboard/components/blocks.vue
+++ monitor_ui3/src/views/dashboard/components/blocks.vue
@@ -4,45 +4,29 @@
       <el-col :span="8">
         <draggable v-model="list" v-bind="dragOptions" :move="onMove" class="list-group" tag="ul" @start="isDragging=true" @end="isDragging=false">
           <li v-for="element in list" :key="element.order" class="list-group-item">
-            {{ element.name }}
-            <span class="badge">{{ element.order }}</span>
             <component :is="element.comp_name"/>
           </li>
-          <div @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
+          <div v-if="list.length==0" @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
 
         </draggable>
       </el-col>
       <el-col :span="8">
         <draggable v-model="list2" v-bind="dragOptions" :move="onMove" class="list-group" tag="ul" @start="isDragging=true" @end="isDragging=false">
           <li v-for="element in list2" :key="element.order" class="list-group-item">
-            {{ element.name }}
-            <span class="badge">{{ element.order }}</span>
             <component :is="element.comp_name"/>
           </li>
-          <div @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
+          <div v-if="list2.length==0" @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
         </draggable>
       </el-col>
       <el-col :span="8">
         <draggable v-model="list3" v-bind="dragOptions" :move="onMove" class="list-group" tag="ul" @start="isDragging=true" @end="isDragging=false">
           <li v-for="element in list3" :key="element.order" class="list-group-item">
-            {{ element.name }}
-            <span class="badge">{{ element.order }}</span>
             <component :is="element.comp_name"/>
           </li>
-          <div @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
+          <div v-if="list3.length==0" @start="isDragging=false" @end="isDragging=false"><a>添加图表看板</a></div>
         </draggable>
       </el-col>
     </el-row>
-
-    <div class="list-group col-md-3">
-      <pre>{{ listString }}</pre>
-    </div>
-    <div class="list-group col-md-3">
-      <pre>{{ list2String }}</pre>
-    </div>
-    <div class="list-group col-md-3">
-      <pre>{{ list3String }}</pre>
-    </div>
   </div>
 </template>
 
@@ -59,9 +43,9 @@ export default {
   },
   data() {
     return {
-      list: [{ name: 'xx', order: 0, fixed: false, comp_name: 'Host' }],
-      list2: [{ name: 'xx', order: 1, fixed: false, comp_name: 'Event' }],
-      list3: [{ name: 'xx', order: 2, fixed: false, comp_name: 'Host' }],
+      list: [{ name: '看板列1', order: 0, fixed: false, comp_name: 'Host' }],
+      list2: [{ name: '看板列2', order: 1, fixed: false, comp_name: 'Event' }],
+      list3: [{ name: '看板列3', order: 2, fixed: false, comp_name: 'Host' }],
       isDragging: false,
       delayedDragging: false
     }
@@ -74,15 +58,6 @@ export default {
         disabled: false,
         ghostClass: 'ghost'
       }
-    },
-    listString() {
-      return JSON.stringify(this.list, null, 2)
-    },
-    list2String() {
-      return JSON.stringify(this.list2, null, 2)
-    },
-    list3String() {
-      return JSON.stringify(this.list3, null, 2)
     }
   },
   watch: {
