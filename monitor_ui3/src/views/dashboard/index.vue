202ef9b7d01eab2ef6cd00b303c445ef4c114a2a
evoup.dell-notebook <evoex123@gmail.com>
2019-07-28 13:48:07 +0800

    首页添加监控事件

--- monitor_ui3/src/views/dashboard/index.vue
+++ monitor_ui3/src/views/dashboard/index.vue
@@ -14,15 +14,34 @@
             <el-table-column
               prop="name"
               label="项目"
-              width="150"
+              width="180"
               align="center"
             />
-            <el-table-column label="数目" prop="num" width="150" />
+            <el-table-column label="数目" prop="num" width="180" />
           </el-table-column>
 
         </el-table>
       </el-col>
-      <el-col :span="8" />
+      <el-col :span="8">
+        <el-table
+          :data="dataList2"
+          :header-cell-style="discountHeaderStyle1"
+          stripe
+          border
+          tooltip-effect="dark"
+          style="width: 100%"
+        >
+          <el-table-column align="center" label="监控事件">
+            <el-table-column
+              prop="name"
+              label="项目"
+              width="180"
+              align="center"
+            />
+            <el-table-column label="数目" prop="num" width="180" />
+          </el-table-column>
+        </el-table>
+      </el-col>
       <el-col :span="8" />
     </el-row>
   </div>
@@ -47,6 +66,25 @@ export default {
         }, {
           date: '2016-05-03',
           name: '未监控',
+          num: 31
+        }
+      ],
+      dataList2: [
+        {
+          date: '2016-05-03',
+          name: '信息',
+          num: 3
+        }, {
+          date: '2016-05-03',
+          name: '警告',
+          num: 31
+        }, {
+          date: '2016-05-03',
+          name: '严重警告',
+          num: 31
+        }, {
+          date: '2016-05-03',
+          name: '灾难警告',
           num: 31
         }
       ]
