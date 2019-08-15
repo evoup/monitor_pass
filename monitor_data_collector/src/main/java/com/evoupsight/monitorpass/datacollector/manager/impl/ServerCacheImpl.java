540cbef7156965798e09e03dbb8306070c64f237
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-08-16 01:16:45 +0800

    np problem

--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/manager/impl/ServerCacheImpl.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/manager/impl/ServerCacheImpl.java
@@ -6,6 +6,7 @@ import com.evoupsight.monitorpass.datacollector.dao.model.ServerExample;
 import com.evoupsight.monitorpass.datacollector.manager.ServerCache;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * @author evoup
  */
 @SuppressWarnings("SpringJavaAutowiredMembersInspection")
+@Component
 public class ServerCacheImpl implements ServerCache {
     @Autowired
     private ServerMapper serverMapper;
