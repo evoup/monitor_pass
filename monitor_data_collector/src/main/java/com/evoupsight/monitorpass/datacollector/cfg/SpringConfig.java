d1476e0e76eb32b2cce4c081b7bc95998ad26ab1
evoup.dell-notebook <evoex123@gmail.com>
2019-08-18 20:48:40 +0800

    初步解決guava cache在netty中使用的問題

--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/cfg/SpringConfig.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/cfg/SpringConfig.java
@@ -213,18 +213,23 @@ public class SpringConfig {
         return Executors.newFixedThreadPool(5);
     }
 
-    @Bean(name = "guava_cache")
-    public LoadingCache<String, String> guavaCacheBean() {
-        CacheLoader<String, String> loader;
-        loader = new CacheLoader<String, String>() {
-            @Override
-            public String load(String key) {
-                return key.toUpperCase();
-            }
-        };
-        return CacheBuilder.newBuilder()
-                .maximumSize(10000)
-                .expireAfterAccess(30L, TimeUnit.SECONDS)
-                .build(loader);
-    }
+    /**
+     * netty自动注入失败，获取bean的方式没有成功，只能用变通方案
+     * @See com.evoupsight.monitorpass.datacollector.sender.Sender#init()
+     */
+    //
+//    @Bean(name = "guava_cache")
+//    public LoadingCache<String, String> guavaCacheBean() {
+//        CacheLoader<String, String> loader;
+//        loader = new CacheLoader<String, String>() {
+//            @Override
+//            public String load(String key) {
+//                return key.toUpperCase();
+//            }
+//        };
+//        return CacheBuilder.newBuilder()
+//                .maximumSize(10000)
+//                .expireAfterAccess(30L, TimeUnit.SECONDS)
+//                .build(loader);
+//    }
 }
