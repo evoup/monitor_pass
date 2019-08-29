a736a1f3e6f3aafbd30b4771915853cb09838938
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-08-29 17:39:21 +0800

    update sql

--- monitor_api2/init.sql
+++ monitor_api2/init.sql
@@ -64,7 +64,7 @@ INSERT INTO `diagram` (`id`,`name`,`width`,`height`) VALUES (2,'memory usage','1
 INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (1,1,2);
 INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (2,1,26);
 INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (3,1,27);
-INSERT INTO `monitor`.`diagram_item` (`diagram_id`, `item_id`) VALUES ('2', '23');
+INSERT INTO `monitor`.`diagram_item` (`diagram_id`, `item_id`) VALUES (2, 23);
 
 
 INSERT INTO `monitor`.`general_config` (`send_warn`) VALUES ('1');
