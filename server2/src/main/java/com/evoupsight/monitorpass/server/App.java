9697b34a94ca88d716262e907aeffe3bcb6f1745
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-09-02 16:04:51 +0800

    update

--- server2/src/main/java/com/evoupsight/monitorpass/server/App.java
+++ server2/src/main/java/com/evoupsight/monitorpass/server/App.java
@@ -2,21 +2,22 @@ package com.evoupsight.monitorpass.server;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author evoup
  */
 @EnableScheduling
-@SpringBootApplication(scanBasePackages="com.evoupsight.monitorpass.server")
+@SpringBootApplication(scanBasePackages = "com.evoupsight.monitorpass.server")
 @MapperScan("com.evoupsight.monitorpass.server.dao.mapper")
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(App.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        new SpringApplicationBuilder(App.class).bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
