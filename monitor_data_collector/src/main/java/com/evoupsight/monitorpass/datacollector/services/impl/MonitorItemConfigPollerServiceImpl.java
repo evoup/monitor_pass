46642ee204f9592736ce1270b3f9a135793b58d2
evoup.dell-notebook <evoex123@gmail.com>
2019-09-04 22:35:01 +0800

    调用jsonrpc

--- monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/services/impl/MonitorItemConfigPollerServiceImpl.java
+++ monitor_data_collector/src/main/java/com/evoupsight/monitorpass/datacollector/services/impl/MonitorItemConfigPollerServiceImpl.java
@@ -1,11 +1,17 @@
 package com.evoupsight.monitorpass.datacollector.services.impl;
 
+import com.evoupsight.monitorpass.datacollector.constants.RpcConstant;
 import com.evoupsight.monitorpass.datacollector.services.SnmpPollerService;
+import com.googlecode.jsonrpc4j.JsonRpcClient;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -39,8 +45,34 @@ public class MonitorItemConfigPollerServiceImpl {
         }
     }
 
-    private void configUpdatePoll() {
+    public void configUpdatePoll() {
         LOG.info("monitor item rpc client updater");
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", 8338);
+            JsonRpcClient client = new JsonRpcClient();
+
+            InputStream ips = socket.getInputStream();
+            OutputStream ops = socket.getOutputStream();
+            int reply = client.invokeAndReadResponse("MonitorItemConfig.Update", new Object[]{"memeda"}, int.class, ops, ips);
+            System.out.println("reply: " + reply);
+            if (RpcConstant.SERVER_OK.code.equals(reply)) {
+                System.out.println("调用服务成功");
+            } else {
+                System.out.println("调用服务失败");
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+
     }
 
     private Callable<Boolean> callableTask = () -> {
--- monitor_data_collector/src/test/JsonRpcClientTest.java
+++ monitor_data_collector/src/test/JsonRpcClientTest.java
@@ -1,10 +1,5 @@
-import com.evoupsight.monitorpass.datacollector.constants.RpcConstant;
-import com.googlecode.jsonrpc4j.JsonRpcClient;
+import com.evoupsight.monitorpass.datacollector.services.impl.MonitorItemConfigPollerServiceImpl;
 import org.junit.Test;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 
 /**
  * https://www.cnblogs.com/geomantic/p/4751859.html
@@ -13,20 +8,7 @@ public class JsonRpcClientTest {
     @Test
     public void aTest() {
         try {
-            Socket socket = new Socket("127.0.0.1", 8338);
-            JsonRpcClient client = new JsonRpcClient();
-
-            InputStream ips = socket.getInputStream();
-            OutputStream ops = socket.getOutputStream();
-
-            int reply = client.invokeAndReadResponse("MonitorItemConfig.Update", new Object[]{"memeda"}, int.class, ops, ips);
-
-            System.out.println("reply: " + reply);
-            if (RpcConstant.SERVER_OK.code.equals(reply)) {
-                System.out.println("调用服务成功");
-            } else {
-                System.out.println("调用服务失败");
-            }
+            new MonitorItemConfigPollerServiceImpl().configUpdatePoll();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
