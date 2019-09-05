52fcb6930c4247f76c1623c2f943330a963003c0
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-09-05 18:55:57 +0800

    update

--- madmonitor2/go/src/madmonitor2/build_debug_fast.sh
+++ madmonitor2/go/src/madmonitor2/build_debug_fast.sh
@@ -6,15 +6,15 @@ go build madmonitor2.go
 #go build -gcflags "-l -N" --buildmode=plugin -o /usr/local/lib/madmonitor2/procstats.so collectors/procstats.go
 
 TEMP_BUILD_DIR=/tmp/madmonitor2_build/
-mkdir $TEMP_BUILD_DIR || true
+mkdir ${TEMP_BUILD_DIR} || true
 find collectors/ -maxdepth 1 -mindepth 1 -type f -name "*.go" | cut -c12- | parallel -I% --max-args 1 go build -gcflags "-l" --buildmode=plugin -o $TEMP_BUILD_DIR%.so collectors/%
 
 MY_SO=$(find collectors/ -maxdepth 1 -mindepth 1 -type f -name "*.go" | cut -c12-)
-echo $MY_SO
+echo ${MY_SO}
 
  
-for word in $MY_SO; do
-    x=$(basename $word .go)
+for word in ${MY_SO}; do
+    x=$(basename ${word} .go)
     #something like dfstat
-    mv $TEMP_BUILD_DIR$x.go.so $TEMP_BUILD_DIR$x.so || true
+    mv ${TEMP_BUILD_DIR}${x}.go.so ${TEMP_BUILD_DIR}${x}.so || true
 done
