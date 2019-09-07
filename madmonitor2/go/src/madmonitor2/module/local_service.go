e2c8437331cf8da57448893803f26e2acb7d4d9c
evoup.dell-notebook <evoex123@gmail.com>
2019-09-07 20:27:04 +0800

    注册rpc

--- madmonitor2/go/src/madmonitor2/module/local_service.go
+++ madmonitor2/go/src/madmonitor2/module/local_service.go
@@ -12,11 +12,23 @@ import (
     "strings"
 )
 
+/*能够注册的方法必须满足指定的函数签名：
+
+    func (t *T) MethodName(argType T1, replyType *T2) error 。
+
+    入参有且只有两个，第一个是被调用函数的输入参数，第二个是被调用函数的输出参数。 返回值只有一个error类型。这三个参数一个都不能变。*/
+
 // 本地json rpc服务结构，必须有，不然不能注册
 type MonitorItemsConfig struct {
     json string // 这里面其实是json
 }
 
+// 客户端版本，必须有，不然不能注册
+type MonitorAgentVersion struct {
+    version string
+}
+
+// 更新客户端配置的rpc方法
 func (this *MonitorItemsConfig) Update(configStr string, r *int) error {
     // 服务端传来的是对象数组
     keys := make([]inc.MonitorItem, 0)
@@ -28,7 +40,7 @@ func (this *MonitorItemsConfig) Update(configStr string, r *int) error {
         fmt.Printf("items: %v", &keys)
         inc.ConfigCache.Set("monitorItems", configStr, cache.NoExpiration)
         // 写入本地监控项配置文件
-        err = config.WriteJson(inc.PROC_ROOT + "/" + inc.WORK_SUBPATH + inc.MONITOR_ITEMS_CONF_FILE, configStr)
+        err = config.WriteJson(inc.PROC_ROOT+"/"+inc.WORK_SUBPATH+inc.MONITOR_ITEMS_CONF_FILE, configStr)
         if err != nil {
             fmt.Println(err)
         }
@@ -41,9 +53,20 @@ func (this *MonitorItemsConfig) Update(configStr string, r *int) error {
     return nil
 }
 
+// 获取客户端版本号的rpc方法
+func (this *MonitorAgentVersion) Get(_ string, r *string) error {
+    *r = inc.CLIENT_VERSION
+    return nil
+}
+
 // NewUpdateConfigServer jsonRpc服务，接收来自数据收集器发送的监控项配置，并写入缓存，供collector获取，按照系统的设置工作
 func NewUpdateConfigServer() {
     err := rpc.Register(new(MonitorItemsConfig))
+    if err != nil {
+        fmt.Printf("rpc register err: %s", err)
+        return
+    }
+    err = rpc.Register(new(MonitorAgentVersion))
     if err != nil {
         fmt.Printf("rpc register err: %s", err)
         return
