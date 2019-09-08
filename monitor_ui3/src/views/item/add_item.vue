44e72175a6343d4426df1c8839f54032b3fd4493
evoup.dell-notebook <evoex123@gmail.com>
2019-09-08 20:56:46 +0800

    update

--- monitor_ui3/src/views/item/add_item.vue
+++ monitor_ui3/src/views/item/add_item.vue
@@ -58,7 +58,7 @@
         </el-col>
       </el-form-item>
       <el-form-item label="数据单位">
-        <el-col :span="2">
+        <el-col :span="4">
           <el-input v-model="form.unit" placeholder="请输入数据单位"/>
         </el-col>
       </el-form-item>
