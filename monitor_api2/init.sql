c132fc62894a9b1056330e259cc9bd7e250a1f73
evoup.dell-notebook <evoex123@gmail.com>
2019-09-15 00:18:58 +0800

    update

--- monitor_api2/init.sql
+++ monitor_api2/init.sql
@@ -45,7 +45,7 @@ INSERT INTO `monitor`.`item` (`name`, `data_type`, `delay`, `desc`, `error`, `ke
 INSERT INTO `monitor`.`item` (`name`, `data_type`, `delay`, `desc`, `error`, `key`, `multiplier`, `unit`, `host_id`,`template_id`, `delta`) VALUES ('Processor load (15 min average per core)', '0', '60', 'The processor load is calculated as system CPU load divided by number of CPU cores.', '', 'system.cpu.load[percpu,avg15]', '1', '', '0','1', '0');
 
 INSERT INTO `monitor`.`item` (`name`, `data_type`, `delay`, `desc`, `error`, `key`, `multiplier`, `unit`, `host_id`, `template_id`, `delta`) VALUES ('Used disk space on $1', '0', '60', 'Used disk space on $1', '', 'vfs.fs.size[{#FSNAME},pused]', '1', '%', '0', '1', '0');
-
+INSERT INTO `monitor`.`item` (`name`, `data_type`, `delay`, `desc`, `error`, `key`, `multiplier`, `unit`, `host_id`, `template_id`, `delta`) VALUES ('Total threads', '0', '60', 'Total threads', '', 'proc.loadavg.total_threads', '1', '', '0', '1', '0');
 
 INSERT INTO `monitor`.`trigger` (`name`, `expression`, `template_id`) VALUES ('Too many processes on {HOST.NAME}', '{1}>300', '1');
 INSERT INTO `monitor`.`trigger` (`name`, `expression`, `template_id`) VALUES ('Processor load is too high on {HOST.NAME}', '{2}>5', '1');
