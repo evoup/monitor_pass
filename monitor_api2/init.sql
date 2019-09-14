0a351df6a3f446a030f9f42ce838bd5d90fcd34f
evoup.dell-notebook <evoex123@gmail.com>
2019-09-15 00:21:58 +0800

    update

--- monitor_api2/init.sql
+++ monitor_api2/init.sql
@@ -62,7 +62,7 @@ INSERT INTO `monitor`.`function` (`name`, `parameter`, `item_id`, `trigger_id`) 
 
 
 INSERT INTO `diagram` (`id`,`name`,`width`,`height`,`template_id`) VALUES (1,'cpu负载','100%','230','1');
-INSERT INTO `diagram` (`id`,`name`,`width`,`height`,`template_id`) VALUES (2,'内存使用率','100%','230','1');
+INSERT INTO `diagram` (`id`,`name`,`width`,`height`,`template_id`) VALUES (2,'内存使用','100%','230','1');
 INSERT INTO `diagram` (`id`,`name`,`width`,`height`,`template_id`) VALUES (3,'所有进程数','100%','230','1');
 INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (1,1,2);
 INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (2,1,26);
