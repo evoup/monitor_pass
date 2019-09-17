118ca7f2093a4844306f1a7c863c7900b6ef0f56
evoup.dell-notebook <evoex123@gmail.com>
2019-09-18 01:57:48 +0800

    update

--- monitor_api2/init.sql
+++ monitor_api2/init.sql
@@ -89,3 +89,6 @@ INSERT INTO `diagram_item` (`id`,`diagram_id`,`item_id`) VALUES (6,4,29);
 INSERT INTO `monitor`.`general_config` (`send_warn`) VALUES ('1');
 INSERT INTO `monitor`.`notification_mode` (`type`, `name`, `smtp_server`, `smtp_domain`, `smtp_port`, `username`, `passwd`, `enabled`) VALUES ('1', 'email', 'smtp.company.com', 'company.com', '25', 'user', 'passwd', '1');
 INSERT INTO `monitor`.`notification_mode` (`type`, `name`, `wechat_agent_id`, `wechat_corp_id`, `wechat_secret`, `enabled`) VALUES ('2', '企业微信', 'agent_id', 'corp_id', 'secret', '1');
+
+INSERT INTO `monitor`.`server_group` (`name`, `alarm_type`) VALUES ('Linux Servers', '1');
+INSERT INTO `monitor`.`r_template_server_group` (`template_id`, `servergroup_id`) VALUES ('1', '1');
