1864b7ffb84cf7d0fe9aa10af596c51ce2ebf2ca
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-09-20 18:26:42 +0800

    update ui

--- monitor_ui3/src/views/config/add_operation.vue
+++ monitor_ui3/src/views/config/add_operation.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="操作名称">
+      <el-form-item label="操作名称：">
         <el-col :span="8">
           <el-input v-model="form.name" placeholder="请输入操作名称"/>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="通知主题：">
+        <el-col :span="8">
+          <el-input v-model="form.subject" placeholder="请输入通知主题"/>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="通知内容：">
+        <el-col :span="8">
+          <el-input
+            v-model="form.message"
+            class="note"
+            placeholder="请输入通知内容"
+            type="textarea"
+          />
         </el-col>
       </el-form-item>
     </el-form>
@@ -11,9 +26,18 @@
 </template>
 
 <script>
-  export default {
-    name: 'addOperation'
+export default {
+  name: 'AddOperation',
+  data() {
+    return {
+      form: {
+        name: '',
+        subject: '',
+        message: ''
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
