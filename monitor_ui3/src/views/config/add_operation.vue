02d51d9942e57a81f904a12411a571dce12f818f
evoup.dell-notebook <evoex123@gmail.com>
2019-09-22 19:36:24 +0800

    update

--- monitor_ui3/src/views/config/add_operation.vue
+++ monitor_ui3/src/views/config/add_operation.vue
@@ -21,11 +21,32 @@
           />
         </el-col>
       </el-form-item>
+      <el-form-item
+        label="请选择模板："
+      >
+        <el-col :span="8">
+          <el-select
+            v-model="templateSelectModel"
+            placeholder="请选择模板（可选）"
+            style="width: 80%"
+          >
+            <el-option
+              v-for="item in templateListData"
+              :key="item.id"
+              :label="item.name"
+              :aria-selected="true"
+              :value="item.id"
+            />
+          </el-select>
+        </el-col>
+      </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import { template_list } from '../../api/template'
+
 export default {
   name: 'AddOperation',
   data() {
@@ -34,7 +55,20 @@ export default {
         name: '',
         subject: '',
         message: ''
-      }
+      },
+      templateListData: [],
+      templateSelectModel: null
+    }
+  },
+  created() {
+    this.fetchTemplateListData()
+  },
+  methods: {
+    // 获取所有模板列表
+    fetchTemplateListData() {
+      template_list({ page: 1, size: 99999, order: 'asc' }).then(response => {
+        this.templateListData = response.data.items
+      })
     }
   }
 }
