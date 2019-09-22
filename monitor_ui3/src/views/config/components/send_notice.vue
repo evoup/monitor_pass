4a5df9041791afb475f8f2ae5e8c843c6d766bad
evoup.dell-notebook <evoex123@gmail.com>
2019-09-23 00:44:21 +0800

    update

--- monitor_ui3/src/views/config/components/send_notice.vue
+++ monitor_ui3/src/views/config/components/send_notice.vue
@@ -2,12 +2,13 @@
   <div class="param-form">
     <el-form ref="operationForm" :model="operationForm" label-width="120px">
       <el-form-item label="指定的轮次：">
-        <el-col :span="8">
+        <el-col :span="6">
           <el-input v-model="step" placeholder=""/>
         </el-col>
+        <span style="font-size: 10px;color: dimgrey;padding-left:10px;">（a-b代表从第a次到第b次，b为0代表无限次）</span>
       </el-form-item>
       <el-form-item label="发送间隔：">
-        <el-col :span="8">
+        <el-col :span="6">
           <el-input v-model="send_interval" placeholder=""/>
         </el-col>
       </el-form-item>
