fc2a50164470e966aa3cc98460acd06123fba88b
evoup.dell-notebook <evoex123@gmail.com>
2019-10-02 14:51:46 +0800

    update

--- monitor_api2/monitor_web/tasks.py
+++ monitor_api2/monitor_web/tasks.py
@@ -1,17 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
-from paramiko import SSHClient, AutoAddPolicy
+from paramiko import SSHClient, AutoAddPolicy, RSAKey
 
 
 @shared_task
 def add(host, port, username, command):
     if str(command).find('rm ') >= 0 or str(command).find('rm -rf') >= 0:
-        return '该命令被禁用!'
+        return {'out': '该命令被禁用!'}
+    pkey = '/home/evoup/.ssh/id_rsa'
+    key = RSAKey.from_private_key_file(pkey)
     ssh = SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.connect(hostname=host, port=port, username=username, password='xxx', compress=True)
+    # ssh.connect(hostname=host, port=port, username=username, password='xxx', compress=True)
+    ssh.connect(hostname=host, port=port, username=username, pkey=key, compress=True)
     stdin, stdout, stderr = ssh.exec_command(command)
     return {'out': str(stdout.read())}
 
