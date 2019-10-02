67e47b849dcaef1882c7bcf649610cb7712a70d9
evoup.dell-notebook <evoex123@gmail.com>
2019-10-02 23:56:46 +0800

    update

--- monitor_ui3/src/views/server/batch_file_upload.vue
+++ monitor_ui3/src/views/server/batch_file_upload.vue
@@ -1,15 +1,163 @@
 <template>
-  <div class="app-container"/>
+  <div class="app-container">
+    <el-row>
+      <el-col :span="6"><div>
+        <el-tree ref="tree1" :data="tree_data" show-checkbox style="background: transparent"/>
+      </div></el-col>
+      <el-col :span="18"><div>
+        <el-input
+          id="textarea0"
+          v-model="resultModel"
+          placeholder=""
+          type="textarea"/>
+      </div></el-col>
+    </el-row>
+    <!--下方输入框和按钮-->
+    <el-row style="margin-top:20px">
+      <el-col :span="6">
+        <div class="grid-content"/>
+      </el-col>
+      <el-col :span="18">
+        <el-row>
+          <el-col :span="18">
+            <div>
+              <!--原先编辑的地方-->
+              <el-upload
+                ref="upload"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :file-list="fileList"
+                :auto-upload="false"
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/">
+                <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                <div slot="tip" class="el-upload__tip">上传单个不超过50M的文件</div>
+              </el-upload>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <el-input v-model="send_user_input" placeholder="请输入上传的系统用户名" class="send_user"/>
+            <el-button type="success" class="send_button" @click="send_files">上传到服务器</el-button>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import { server_group_list, server_list } from '../../api/server'
+import { batch_send_commands, get_command_result } from '../../api/batch_operation'
+
 export default {
-  name: 'BatchFileUpload'
+  name: 'BatchFileUpload',
+  data() {
+    return {
+      tree_data: [],
+      pageHelp: {
+        page: 1,
+        size: 7,
+        order: 'asc'
+      },
+      sortHelp: {
+        prop: '',
+        order: ''
+      },
+      resultModel: '',
+      commandModel: null,
+      send_user_input: null,
+      fileList: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }]
+    }
+  },
+  watch: {
+    'resultModel': 'scrollToBottom'
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    buildTree(data0, data) {
+      this.tree_data = []
+      for (const i in data0) {
+        const server_group_id = 'server_group|' + data0[i].id
+        const label = data0[i].name
+        const member_servers = []
+        for (const j in data) {
+          const node_label = data[j].hostname
+          member_servers.push({ id: 'server|' + data[j].id, label: node_label })
+        }
+        const server_group_member = { id: server_group_id, label: label, children: member_servers }
+        this.tree_data.push(server_group_member)
+      }
+    },
+    fetchData() {
+      server_group_list().then(response => {
+        const server_groups = response.data.items
+        server_list(Object.assign(this.pageHelp, this.sortHelp)).then(response => {
+          this.buildTree(server_groups, response.data.items)
+        })
+      })
+    },
+    send_files() {
+      var command = this.$refs.editAreaShellComp.code
+      batch_send_commands(this.$refs.tree1.getCheckedNodes(), this.send_user_input, command).then(response => {
+        var tasks = response.data.items
+        for (var i in tasks) {
+          this.wait_command_finish(tasks[i])
+        }
+      })
+    },
+    // 轮训任务结果
+    wait_command_finish(task_id) {
+      get_command_result({ task_id: task_id }).then(response => {
+        if (response.data.item != null) {
+          this.resultModel = this.resultModel + '\n' + response.data.item.name + '执行命令完成，结果如下：\n' + response.data.item.out
+        } else {
+          setTimeout(() => {
+            this.wait_command_finish(task_id)
+          }, 2000)
+        }
+      })
+    },
+    // 执行结果滚动条滚动到底部
+    scrollToBottom: function() {
+      this.$nextTick(() => {
+        var div = document.getElementById('textarea0')
+        div.scrollTop = div.scrollHeight
+      })
+    },
+    submitUpload() {
+      this.$refs.upload.submit()
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview(file) {
+      console.log(file)
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .param-form /deep/ textarea {
-    height: 280px;
+  .app-container /deep/ #textarea0 {
+    height: 380px;
+    background: #1f2d3d;
+    color: chartreuse;
   }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .send_user {
+    width:170px;
+    margin-top:20px;
+    margin-left:20px;
+  }
+  .send_button {
+    width:170px;
+    margin-top:20px;
+    margin-left:20px;
+  }
+
 </style>
