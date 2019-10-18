35f0b1cc3b992c3c0b904a1d1e570537e094b510
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-10-18 21:54:53 +0800

    update

---
+++ testing/src/main/java/jmeter/JavaRequest.java
@@ -0,0 +1,60 @@
+package jmeter;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @author evoup
+ * 测试数据收集器的并发连接数
+ */
+@SuppressWarnings("unused")
+public class JavaRequest extends AbstractJavaSamplerClient {
+
+
+    @Override
+    public Arguments getDefaultParameters() {
+        return super.getDefaultParameters();
+    }
+
+    @Override
+    public void setupTest(JavaSamplerContext context) {
+        super.setupTest(context);
+    }
+
+    public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+        SampleResult result = new SampleResult();
+
+        boolean success = true;
+        result.sampleStart();
+        // Write your test code here.
+        Socket socket = new Socket();
+        SocketAddress address = new InetSocketAddress("172.16.30.220", 8091);
+        try {
+            socket.connect(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+            success = false;
+        } finally {
+            result.sampleEnd();
+            result.setSuccessful(success);
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void teardownTest(JavaSamplerContext context) {
+        super.teardownTest(context);
+    }
+}
